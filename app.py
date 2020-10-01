9ea80ec956a53823643a32cbe13a2544c1ba1d9e
akshay-2210 <62390860+akshay-2210@users.noreply.github.com>
2020-10-01 20:05:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, url_for
+import pickle
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+
+
+
+app=Flask(__name__)
+model_path = 'ipl.h5'
+model = load_model(model_path)
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        bat = str(request.form['Batting team'])
+        bowl = str(request.form['Bowling team'])
+        if bat == bowl:
+            return render_template('index.html',prediction_text='Batting and Bowling team can not be the same.')
+
+            
+        overs = int(request.form['Overs'])
+        balls = int(request.form['Balls'])
+        scored = int(request.form['Runs Scored'])
+        wicket = int(request.form['Wicket left'])
+        required = int(request.form['Required Runs'])
+        
+        over = overs + 0.1*(balls)
+        crr = scored/over
+         
+       
+        probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+        predictions = probability_model.predict([[[overs,balls,scored,wicket,required,crr]]])
+
+        if predictions[0][0]>predictions[0][1]:
+            result = bowl
+            probability = predictions[0][0]
+        else:
+            result = bat
+            probability = predictions[0][1]
+    
+        
+        return render_template('index.html',prediction_text="{} has {}% chance of winning the match".format(result,np.round(probability*100,2)))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
